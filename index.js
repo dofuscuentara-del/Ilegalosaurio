2f1fb7412d98e535b0d3710462b301329a11548b
Ray <dofuscuenta.ra@gmail.com>
2026-02-01 15:16:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 // index.js
 document.addEventListener('DOMContentLoaded', () => {
-  const btnEscanear = document.getElementById('btnEscanear');
-  const btnPanel = document.getElementById('btnPanel');
-
-  btnEscanear.addEventListener('click', () => {
-    // modo asistencia
+  document.getElementById('btnEscanear').onclick = () => {
     localStorage.setItem('scanner_modo', 'asistencia');
-    window.location.href = 'scanner.html';
-  });
+    location.href = 'scanner.html';
+  };
 
-  btnPanel.addEventListener('click', () => {
-    // modo login
+  document.getElementById('btnPanel').onclick = () => {
     localStorage.setItem('scanner_modo', 'login');
-    window.location.href = 'scanner.html';
-  });
+    location.href = 'scanner.html';
+  };
 });
