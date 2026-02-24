a6f4b982840ce26194e1ad8a61d6eea0930b9dc2
Ray <dofuscuenta.ra@gmail.com>
2026-02-23 20:10:53 -0500

    Reorganize code structure in index.js

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 
-const empleado_id = localStorage.getItem("empleado_id");
-const rol = localStorage.getItem("rol");
 
-if (empleado_id && rol) {
-  if (rol === "admin") {
-    location.replace("panel_admin.html");
-  } else if (rol === "empleado") {
-    location.replace("panel_empleado.html");
-  }
-}
 
 // index.js
 document.addEventListener('DOMContentLoaded', () => {
   const btnEscanear = document.getElementById('btnEscanear');
   const btnPanel = document.getElementById('btnPanel');
+
 
   btnEscanear.addEventListener('click', () => {
     // modo asistencia
@@ -28,5 +20,15 @@ btnPanel.addEventListener('click', () => {
   localStorage.setItem('scanner_modo', 'login');
   window.location.href = 'scanner.html';
 });
+  
+const empleado_id = localStorage.getItem("empleado_id");
+const rol = localStorage.getItem("rol");
 
+if (empleado_id && rol) {
+  if (rol === "admin") {
+    location.replace("panel_admin.html");
+  } else if (rol === "empleado") {
+    location.replace("panel_empleado.html");
+  }
+}
 
